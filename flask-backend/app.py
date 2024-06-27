c0c50a6ab31579cf39d5ad9776f4067497527401
Juwang110 <Juwang110@gmail.com>
2024-06-27 10:25:26 -0400

    Adding MySQL support

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -1,13 +1,50 @@
-from flask import Flask, jsonify, send_from_directory
+from dotenv import load_dotenv
+from flask import Flask, jsonify, send_from_directory, request
 from flask_cors import CORS
+from flask_mysqldb import MySQL
+
 import os
 
 app = Flask(__name__, static_folder='../../build')
 CORS(app)  # Allow CORS for all origins
+load_dotenv()
+# MySQL configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = os.getenv("DB_USER")
+app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_ROOT_PASSWORD")
+app.config['MYSQL_DB'] = os.getenv("DB_NAME")
 
-@app.route('/api/data')
+# Initialize MySQL
+mysql = MySQL(app)
+
+@app.route('/api/data', methods=['GET'])
 def get_data():
-    return jsonify({'message': 'Hello from Flask!'})
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM users') 
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
+
+@app.route('/test_mysql')
+def test_mysql():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT VERSION()")
+    version = cur.fetchone()
+    cur.close()
+    return jsonify({"mysql_version": version[0]})
+
+@app.route('/api/add_user', methods=['POST'])
+def add_user():
+    username = 'test'
+    password = 'test'
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+        mysql.connection.commit()
+        cur.close()
+        return jsonify({'message': 'User added successfully!'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -2,19 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:5000/api/data")
-      .then((response) => {
-        setMessage(response.data.message);
-      })
-      .catch((error) => {
-        console.error("Error fetching data from backend:", error);
-      });
-  }, []);
-
   return (
     <div>
       <h1>{message}</h1>
