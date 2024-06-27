6233eb0a5787f5cd6307a25b4d11eba542312847
Juwang110 <Juwang110@gmail.com>
2024-06-27 14:37:45 -0400

    Incorporating flowbite UI

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Button } from "flowbite-react";
 
 function App() {
   return (
-    <div>
-      <h1>{message}</h1>
+    <div className="flex flex-wrap gap-2">
+      <Button>Default</Button>
+      <Button color="blue">Blue</Button>
+      <Button color="gray">Gray</Button>
+      <Button color="dark">Dark</Button>
+      <Button color="light">Light</Button>
+      <Button color="success">Success</Button>
+      <Button color="failure">Failure</Button>
+      <Button color="warning">Warning</Button>
+      <Button color="purple">Purple</Button>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}", flowbite.content()],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbite.plugin()],
 };
