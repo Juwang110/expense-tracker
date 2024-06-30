78289a463acf3d3664709339d122bfa3b7207173
Juwang110 <Juwang110@gmail.com>
2024-06-29 21:30:22 -0400

    Adding contact me page with automatic email sending flask mail support

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,12 @@
 import React from "react";
+import userIcon from "../assets/userIcon.png";
 
 export default function About() {
-  return <h1>About</h1>;
+  return (
+    <img
+      src={userIcon}
+      alt="Description of image"
+      className="w-64 h-auto rounded-lg py-10"
+    />
+  );
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,17 @@
 import React from "react";
+import NavigationBar from "../components/NavigationBar";
+import { ContactMe } from "../components/ContactMe";
 
 export default function Contact() {
-  return <h1>Contact</h1>;
+  return (
+    <div className="flex flex-col min-h-screen px-20">
+      <NavigationBar />
+      <div class="flex flex-col justify-center text-center py-3 items-center">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-950 dark:text-white">
+          Contact Me
+        </h1>
+        <ContactMe></ContactMe>
+      </div>
+    </div>
+  );
 }
