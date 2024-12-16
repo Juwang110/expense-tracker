12a5e46ccafcb6d0c5a8f7d0eb5c4154b80c0b1c
Justin Wang <Juwang110@gmail.com>
2024-12-16 11:28:05 -0500

    update pytest

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -6,10 +6,6 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_serve(client):
-    response = client.get('/')
-    assert response.status_code == 200
-
 def test_test_route(client):
     response = client.get('/test')
     assert response.status_code == 200
